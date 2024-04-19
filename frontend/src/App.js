3a85f242c125a7a0c1899280acc8f46ef68a32a0
Sauravt25 <tiwarisaurav07@gmail.com>
2024-04-19 07:36:55 +0530

    Till product render on Ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 const App = () => {
   return (
-    <div>Welcome to home page</div>
+    <>
+    <Header />
+    <main className='py-3'>
+      <Container>
+        <HomeScreen />
+      </Container>
+    </main>
+    <Footer />
+    </>
   )
 }
 
